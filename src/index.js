77e314b7a2bb76de190ea80a3660c90b6af175ac
Chiyo T N <chiyo@mac-rock-rowski.local>
2020-01-18 20:57:32 -0800

    Implement mortgage calculator form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    async handleSubmit(event) {
+        const form = new FormData();
+        form.set('PropertyPrice', 10000);
+        form.set('DownPayment', event.target.DownPayment.value);
+        form.set('AnnualInterestRate', event.target.AnnualInterestRate.value);
+        form.set('AmortizationPeriod', event.target.AmortizationPeriod.value);
+        form.set('PaymentSchedule', event.target.PaymentSchedule.value);
+
+        alert(form["PropertyPrice"]);
+
+        const response = await axios.post(
+            "http://localhost:8000/calculate",
+            form,
+            { headers: { 'Content-Type': 'x-www-form-urlencoded' }})
+
+        console.log(await response.data);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Property price:
+                        <input type="text" name="PropertyPrice" defaultValue="10000"/>
+                    </label>
+                    <br/><br/>
+                    <label>
+                        Down payment:
+                        <input type="text" name="DownPayment" defaultValue="1000"/>
+                    </label>
+                    <br/><br/>
+                    <label>
+                        Annual interest rate:
+                        <input type="text" name="AnnualInterestRate" defaultValue="3"/>
+                    </label>
+                    <br/><br/>
+                    <label>
+                        Amortization period:
+                        <select name="AmortizationPeriod">
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="25">25</option>
+                            <option value="30">30</option>
+                        </select>
+                    </label>
+                    <br/><br/>
+                    <label>
+                        Payment schedule:
+                        <select name="PaymentSchedule">
+                            <option value="1">Accelerated bi-weekly</option>
+                            <option value="2">Bi-weekly</option>
+                            <option value="3">Monthly</option>
+                        </select>
+                    </label>
+                    <br/><br/>
+                    <input type="submit" value="Submit" />
+                </form>
+
+                <div>
+                    <strong name="result"></strong>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Form />,
+    document.getElementById('root')
+  );
