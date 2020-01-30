fd230b63d3dabcc49295bb95f8da66d98f3e6f44
Chiyo T N <chiyo.nishio@gmail.com>
2020-01-30 09:29:02 -0800

    Tweak layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MyForm extends React.Component {
@@ -16,29 +15,30 @@ class MyForm extends React.Component {
         event.preventDefault();
 
         // TODO: there must be a better way to do this
-        var query = 'http://localhost:8080/calculate?' + 
-        'propertyPrice=' + event.target.elements.propertyPrice.value + 
-        '&downPayment=' + event.target.elements.downPayment.value + 
-        '&annualInterestPercentage=' + event.target.elements.annualInterestPercentage.value + 
-        '&amortizationPeriod=' + event.target.elements.amortizationPeriod.value + 
+        var query = 'http://localhost:8080/calculate?' +
+        'propertyPrice=' + event.target.elements.propertyPrice.value +
+        '&downPayment=' + event.target.elements.downPayment.value +
+        '&annualInterestPercentage=' + event.target.elements.annualInterestPercentage.value +
+        '&amortizationPeriod=' + event.target.elements.amortizationPeriod.value +
         '&paymentSchedule=' + event.target.elements.paymentSchedule.value
 
         axios.get(query)
         .then((res) => {
             this.setState({
-                mortgage: 'Payment per payment schedule is : ' + res.data.mortgage, 
+                mortgage: 'Payment per payment schedule is : $' + res.data.mortgage,
                 error: ''
             })
         })
         .catch((err) => {
             if (err.response) {
-                // <Alert variant='danger'>
-                //     {err.response.data}
-                // </Alert>
-                console.log(err.response.data);
                 this.setState({
-                    mortgage: '', 
+                    mortgage: '',
                     error: err.response.data
+                })
+            } else {
+                this.setState({
+                    mortgage: '',
+                    error: 'Something went wrong :('
                 })
             }
         })
@@ -47,55 +47,59 @@ class MyForm extends React.Component {
     render() {
         return (
             <div>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="propertyPrice">
-                        <Form.Label>Property price (CAD)</Form.Label>
-                        <Form.Control type="number" step="0.01" defaultValue="300000" />
-                    </Form.Group>
+                <Container>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="propertyPrice">
+                            <Form.Label>Property price (CAD)</Form.Label>
+                            <Form.Control type="number" step="0.01" defaultValue="300000" />
+                        </Form.Group>
 
-                    <Form.Group controlId="downPayment">
-                        <Form.Label>Down payment (CAD)</Form.Label>
-                        <Form.Control type="number" step="0.01" defaultValue="50000" />
-                    </Form.Group>
+                        <Form.Group controlId="downPayment">
+                            <Form.Label>Down payment (CAD)</Form.Label>
+                            <Form.Control type="number" step="0.01" defaultValue="50000" />
+                        </Form.Group>
 
-                    <Form.Group controlId="annualInterestPercentage">
-                        <Form.Label>Annual interest rate (%)</Form.Label>
-                        <Form.Control type="number" defaultValue="3.00" />
-                    </Form.Group>
+                        <Form.Group controlId="annualInterestPercentage">
+                            <Form.Label>Annual interest rate (%)</Form.Label>
+                            <Form.Control type="number" defaultValue="3.00" />
+                        </Form.Group>
 
-                    <Form.Group controlId="amortizationPeriod">
-                        <Form.Label>Amortization period (years)</Form.Label>
-                        <Form.Control as="select">
-                            <option value="5">5</option>
-                            <option value="10">10</option>
-                            <option value="15">15</option>
-                            <option value="20">20</option>
-                            <option value="25">25</option>
-                            <option value="30">30</option>
-                        </Form.Control>
-                    </Form.Group>
+                        <Form.Group controlId="amortizationPeriod">
+                            <Form.Label>Amortization period (years)</Form.Label>
+                            <Form.Control as="select">
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="20">20</option>
+                                <option value="25">25</option>
+                                <option value="30">30</option>
+                            </Form.Control>
+                        </Form.Group>
 
-                    <Form.Group controlId="paymentSchedule">
-                        <Form.Label>Payment schedule</Form.Label>
-                        <Form.Control as="select">
-                            <option value="acceleratedBiWeekly">Accelerated bi-weekly</option>
-                            <option value="biWeekly">Bi-weekly</option>
-                            <option value="monthly">Monthly</option>
-                        </Form.Control>
-                    </Form.Group>
+                        <Form.Group controlId="paymentSchedule">
+                            <Form.Label>Payment schedule</Form.Label>
+                            <Form.Control as="select">
+                                <option value="acceleratedBiWeekly">Accelerated bi-weekly</option>
+                                <option value="biWeekly">Bi-weekly</option>
+                                <option value="monthly">Monthly</option>
+                            </Form.Control>
+                        </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
 
-                <div>
-                    <strong name="mortgage">{this.state.mortgage}</strong>
-                </div>
+                    <br/>
 
-                <div>
-                    <strong color="red" name="error">{this.state.error}</strong>
-                </div>
+                    <div>
+                        <strong name="mortgage">{this.state.mortgage}</strong>
+                    </div>
+
+                    <div>
+                        <strong color="red" name="error">{this.state.error}</strong>
+                    </div>
+                </Container>
             </div>
         );
     }
